6b66c0d9c60356d2643c64e1a1e0fd1aeb3f8838
jescarcon <jesuscardenas2000@gmail.com>
2024-06-16 19:54:31 +0200

    Backend path updated

--- src/FrontEnd/src/constants.js
+++ src/FrontEnd/src/constants.js
@@ -1,6 +1,6 @@
 // src/FrontEnd-React/src/constants.js
 const isProduction = 1;
-export const BASE_API_URL = isProduction ? "https://oceania-backend.onrender.com" : "http://localhost:8000";
+export const BASE_API_URL = isProduction ? "https://mbt-club-backend.onrender.com" : "http://localhost:8000";
                                                  //   1                                                      0
 export const ACCESS_TOKEN = "access";
 export const REFRESH_TOKEN = "refresh";
