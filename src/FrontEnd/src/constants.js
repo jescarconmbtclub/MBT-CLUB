7fa7d174521bf463069bb2f641f67e54a4764256
jescarcon <jesuscardenas2000@gmail.com>
2024-06-17 09:03:45 +0200

    1 fix

--- src/FrontEnd/src/constants.js
+++ src/FrontEnd/src/constants.js
@@ -1,5 +1,5 @@
 // src/FrontEnd-React/src/constants.js
-const isProduction = 0;
+const isProduction = 1;
 export const BASE_API_URL = isProduction ? "https://mbt-club-backend.onrender.com" : "http://localhost:8000";
                                                  //   1                                                      0
 export const ACCESS_TOKEN = "access";
